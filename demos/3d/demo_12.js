d7b8827d24f38e2e261825b8140df99e98614535
gonnavis <gonnavis@163.com>
2022-05-25 07:22:22 +0800

    dev: log rotation.

--- demos/3d/demo_12.js
+++ demos/3d/demo_12.js
@@ -4,7 +4,8 @@ var chain = new FIK.Chain3D();
 var base = new FIK.Bone3D( new FIK.V3(0, 1, 0), new FIK.V3(15, 0, 0) );
 chain.addBone(base);
 // chain.setHingeBaseboneConstraint( 'global', new FIK.V3(0,1,0), 0, 0, new FIK.V3(1,0,0));
-chain.setRotorBaseboneConstraint( 'global', FIK.X_AXE, 90);
+// chain.setRotorBaseboneConstraint( 'global', FIK.X_AXE, 90);
+chain.setRotorBaseboneConstraint( 'global', FIK.X_AXE, 0);
 chain.addConsecutiveHingedBone( defaultBoneDirection, defaultBoneLength, 'local', FIK.Y_AXE, 150, 0, FIK.Z_AXE );
 
 solver.add( chain, target, true );
